91bc03861871051bfccd7c1872cb94560806e93c
hovahyii <hovahyii@gmail.com>
2023-12-26 01:40:24 +0800

    update blog

--- next.config.js
+++ next.config.js
@@ -1,17 +1,21 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+
+
+const nextConfig = {
     env: {
         SUPABASE_URL: process.env.SUPABASE_URL,
         SUPABASE_KEY: process.env.SUPABASE_KEY,
-
     },
     images: {
         domains: [
         'images.squarespace-cdn.com', 
         'th.bing.com', 
         'media.timeout.com',
-        'assets.traveltriangle.com'],
+        'assets.traveltriangle.com',
+        'images.unsplash.com'],
       },
-
+      reactStrictMode: true
+  
   }
 
+  module.exports = nextConfig
