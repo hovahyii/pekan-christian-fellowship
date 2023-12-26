340e817aac66f218679bc48dace83b1b7cd70c20
hovahyii <hovahyii@gmail.com>
2023-12-27 01:28:20 +0800

    update backbutton
    Signed-off-by: hovahyii <hovahyii@gmail.com>

---
+++ app/components/BackButton.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+
+export const BackButton = () => {
+  const goBack = () => {
+    window.history.back();
+  };
+
+  const buttonStyle: React.CSSProperties = {
+    position: 'fixed',
+    top: '20px',
+    left: '20px',
+    backgroundColor: '#007bff',
+    color: 'white',
+    borderRadius: '12%',
+    padding: '10px',
+    cursor: 'pointer',
+    fontSize: '24px',
+    width: '80px',
+    zIndex: 10,
+  };
+
+  return (
+    <div style={buttonStyle as React.CSSProperties} onClick={goBack}>
+      <FontAwesomeIcon icon={faArrowLeft} />
+      &nbsp;Back
+    </div>
+  );
+};
