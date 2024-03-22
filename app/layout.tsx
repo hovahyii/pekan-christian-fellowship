d0c5f5c5884b2b5560c7f0cdf463341a708626b3
hovahyii <hovahyii@gmail.com>
2024-03-22 17:29:43 +0800

    update meta
    Signed-off-by: hovahyii <hovahyii@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,18 @@ import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
-
+export const metadata = {
+  applicationName: 'Pekan Chrisitian Community',
+  referrer: 'origin-when-cross-origin',
+  keywords: ['PCC', 'Pekan Chrisitian Community', 'Pekan Christian', 'Christian in Pekan'],
+  authors: [{ name: 'Hovah', url: 'https://hovahyii.vercel.app/'}],
+  creator: 'Hovah Yii',
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
+}
 
 export default function RootLayout({
   children,
